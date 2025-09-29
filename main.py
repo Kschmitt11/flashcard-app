0272a1abc081f08ff0a1c98246d03de9757563db
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-29 11:50:23 -0500

    Saves progress

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from tkinter import *
 import random
+
+import pandas
 import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
+to_learn = {}
 
 # Read CSV using pandas and convert to dictionary
-data = pd.read_csv("data/spanish_to_english.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("data/spanish_to_english.csv")
+    to_learn = data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 curr_card = {}
 
 # Functions
 def next_card():
     canvas.itemconfig(card_id, image=flashcard_front)
-    global curr_card
+    global curr_card, flip_timer
+    window.after_cancel(flip_timer)
     curr_card = random.choice(to_learn)
     canvas.itemconfig(title_id, text="Spanish", fill="black")
     canvas.itemconfig(word_id, text=curr_card["Spanish"], fill="black")
-    window.after(3000, flip_card)
+    flip_timer = window.after(3000, flip_card)
 
 def flip_card():
     canvas.itemconfig(card_id, image=flashcard_back)
     canvas.itemconfig(title_id, text="English", fill="white")
     canvas.itemconfig(word_id, text=curr_card["English"], fill="white")
 
+def is_known():
+    to_learn.remove(curr_card)
+    content = pandas.DataFrame(to_learn)
+    content.to_csv("data/words_to_learn.csv")
+    next_card()
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 canvas = Canvas(width=800, height=526)
 
 
@@ -50,7 +66,7 @@ wrong_mark = PhotoImage(file="./images/wrong.png")
 
 
 # Buttons
-right_button = Button(image=check_mark, highlightthickness=0, command=next_card)
+right_button = Button(image=check_mark, highlightthickness=0, command=is_known)
 right_button.grid(row=1, column=1)
 wrong_button = Button(image=wrong_mark, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
