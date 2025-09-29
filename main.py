48e1c17a071aff2772e86d16d557add4ef6b3a90
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-28 20:20:36 -0500

    Base code, flip card functionality

---
+++ main.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import random
+import pandas as pd
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# Read CSV using pandas and convert to dictionary
+data = pd.read_csv("data/spanish_to_english.csv")
+to_learn = data.to_dict(orient="records")
+
+curr_card = {}
+
+# Functions
+def next_card():
+    canvas.itemconfig(card_id, image=flashcard_front)
+    global curr_card
+    curr_card = random.choice(to_learn)
+    canvas.itemconfig(title_id, text="Spanish", fill="black")
+    canvas.itemconfig(word_id, text=curr_card["Spanish"], fill="black")
+    window.after(3000, flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_id, image=flashcard_back)
+    canvas.itemconfig(title_id, text="English", fill="white")
+    canvas.itemconfig(word_id, text=curr_card["English"], fill="white")
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+
+
+# Flashcard Images
+flashcard_front = PhotoImage(file="./images/card_front.png")
+flashcard_back = PhotoImage(file="./images/card_back.png")
+
+# Canvas settings
+card_id = canvas.create_image(400,263, image=flashcard_front)
+canvas.grid(row=0, column=0, columnspan=2)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+title_id = canvas.create_text(400, 150, text="Title", font=("Courier", 40, "italic"))
+word_id = canvas.create_text(400, 263, text="Word", font=("Courier", 60, "bold"))
+
+# Images
+check_mark = PhotoImage(file="./images/right.png")
+wrong_mark = PhotoImage(file="./images/wrong.png")
+
+
+
+# Buttons
+right_button = Button(image=check_mark, highlightthickness=0, command=next_card)
+right_button.grid(row=1, column=1)
+wrong_button = Button(image=wrong_mark, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0)
+
+next_card()
+
+
+
+window.mainloop()
+
